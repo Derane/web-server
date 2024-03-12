4d5edc31d3af90c8ac31c6b17db7d026fb2b4de2
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2024-03-12 16:02:53 +0200

    Initial commit

---
+++ src/test/java/com/vladick/project/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladick.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
